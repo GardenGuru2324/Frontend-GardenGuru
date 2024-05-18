e52e01960112c1d2484d2cf31e3803501485aa4e
MilanMareels <milan.mareels@gmail.com>
2024-05-18 12:49:02 +0200

    added mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<>
+			<div className="flex justify-center items-center h-screen w-screen">
+				<Button variant="contained" color="success" size="large" startIcon={<DeleteIcon />}>
+					garden Guru
+				</Button>
+			</div>
+		</>
+	);
 }
