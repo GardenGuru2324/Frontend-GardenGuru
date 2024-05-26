b43902dcaa585623cb19512cf08f9084e8839968
MilanMareels <milan.mareels@gmail.com>
2024-05-26 12:55:26 +0200

    fixed env

--- src/services/plants/plants.ts
+++ src/services/plants/plants.ts
@@ -2,6 +2,5 @@ import axios from 'axios';
 
 export const getPlants = async () => {
 	const response = await axios.get(`https://backend-api-garden-guru.vercel.app/api/v1/plants`);
-
 	return response.data;
 };
