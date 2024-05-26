4e003766115c1ba8c9355516147c3a532b1efff0
MilanMareels <milan.mareels@gmail.com>
2024-05-26 17:02:32 +0200

    first test for preprod

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/homePage';
 
 export default function App() {
 	const queryClient = new QueryClient();
+
 	return (
 		<div className="flex justify-between flex-col items-center h-screen w-full">
 			<QueryClientProvider client={queryClient}>
