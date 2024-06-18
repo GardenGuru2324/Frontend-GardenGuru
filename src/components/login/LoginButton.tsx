9dbf0488d9d25edb880b6b765865406556dec414
MilanMareels <milan.mareels@gmail.com>
2024-06-18 14:17:34 +0200

    Prettier fixed

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -1,26 +1,26 @@
 import { Button } from "@mui/material";
 
 interface LoginButtonProps {
-	handleLogin: (event: React.FormEvent) => void;
+  handleLogin: (event: React.FormEvent) => void;
 }
 
 const LoginButton = ({ handleLogin }: LoginButtonProps) => {
-	return (
-		<Button
-		    variant="contained"
-			sx={{ 
-				width: '100%', 
-				backgroundColor: '#1A4D2E', 
-				borderRadius: 2,
-				'&:hover': {
-					backgroundColor: '#14522C',
-				},
-			}}
-			onClick={(e) => handleLogin(e)}
-		>
-			Sign In
-		</Button>
-	);
+  return (
+    <Button
+      variant="contained"
+      sx={{
+        width: "100%",
+        backgroundColor: "#1A4D2E",
+        borderRadius: 2,
+        "&:hover": {
+          backgroundColor: "#14522C",
+        },
+      }}
+      onClick={(e) => handleLogin(e)}
+    >
+      Sign In
+    </Button>
+  );
 };
 
 export default LoginButton;
--- src/components/login/SignUpButton.tsx
+++ src/components/login/SignUpButton.tsx
@@ -1,21 +1,21 @@
 import { Button } from "@mui/material";
 
 const SignUpButton = () => {
-	return (
-		<Button
-		    variant="contained"
-			sx={{ 
-				width: '100%', 
-				backgroundColor: '#1A4D2E', 
-				borderRadius: 2,
-				'&:hover': {
-					backgroundColor: '#14522C',
-				},
-			}}
-		>
-			Sign Up
-		</Button>
-	);
+  return (
+    <Button
+      variant="contained"
+      sx={{
+        width: "100%",
+        backgroundColor: "#1A4D2E",
+        borderRadius: 2,
+        "&:hover": {
+          backgroundColor: "#14522C",
+        },
+      }}
+    >
+      Sign Up
+    </Button>
+  );
 };
 
 export default SignUpButton;
