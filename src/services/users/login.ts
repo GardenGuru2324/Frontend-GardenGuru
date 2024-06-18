60ca551a1b62f82c1a7ee67ae873db74e0a8e98c
MilanMareels <milan.mareels@gmail.com>
2024-06-18 13:49:03 +0200

    code clean up step onme

--- src/services/users/login.ts
+++ src/services/users/login.ts
@@ -17,10 +17,8 @@ export interface LoginError {
 export const postLogin = async (email: string, password: string): Promise<LoginResponse | LoginError> => {
 	try {
 		const response = await axios.post(`${baseUrl}/login`, { email, password });
-		
 		return response.data as LoginResponse;
 	} catch (error: any) {
-		console.log(baseUrl);
 		if (axios.isAxiosError(error) && error.response) {
 			return {
 				error: true,
