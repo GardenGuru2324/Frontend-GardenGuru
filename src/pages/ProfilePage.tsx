7c35058775e0d16a3b281a9e779fe2eed4b879ff
MilanMareels <milan.mareels@gmail.com>
2024-06-24 15:14:44 +0200

    PR feedback done

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -2,12 +2,13 @@ import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { routerEnum } from "../routes/routesEnum";
 
 export default function ProfilePage() {
   const { logout } = useContext(AuthContext);
   return (
     <div className="flex justify-center items-center flex-wrap h-screen">
-      <Link to="/">
+      <Link to={routerEnum.LOGIN}>
         <Button variant="contained" onClick={() => logout()}>
           Logout
         </Button>
