21a234df5bbd8d03e89800a77775f6f0e7d3c3eb
MilanMareels <milan.mareels@gmail.com>
2024-08-01 12:11:07 +0200

    added link to plant card for navigation

--- src/pages/myPlantDetails.tsx
+++ src/pages/myPlantDetails.tsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
+
 export default function MyPlantsDetailPage() {
+  const { plantId } = useParams();
   return (
     <div className="m-auto">
-      <p>MyPlantsDetailPage</p>
+      <p>MyPlantsDetailPage : {plantId}</p>
     </div>
   );
 }
