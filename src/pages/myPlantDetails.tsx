45ce071210445e73367e5dafe52a273ae30e85ec
MilanMareels <milan.mareels@gmail.com>
2024-08-01 15:24:10 +0200

    added card contenet for image, div not working idk why

--- src/pages/myPlantDetails.tsx
+++ src/pages/myPlantDetails.tsx
@@ -4,10 +4,12 @@ import Loading from "../components/ui/loading";
 import ErrorPage from "../components/errors/errorPage";
 import { BottomDetailSection } from "../components/myPlantDetails/bottomDetailSection";
 import { ChevronLeft } from "lucide-react";
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 
 export default function MyPlantsDetailPage() {
   const { plantId } = useParams();
   const { myPlant, isError, isLoading, error } = useMyPlantsDetails(plantId!);
+  const plantFoto: string | undefined = myPlant?.plantImage;
 
   if (isLoading) {
     return (
@@ -27,16 +29,47 @@ export default function MyPlantsDetailPage() {
 
   return (
     <div className="flex flex-col justify-between items-center min-h-svh h-full w-full">
-      <div className="flex justify-between items-center w-full p-5 mt-2">
+      <div className="flex justify-between items-center w-full p-5 mt-2 relative max-w-[1400px]">
         <Link to="/myPlants">
           <div className="flex-shrink-0">
             <ChevronLeft size={35} />
           </div>
         </Link>
-        <div className="flex-grow text-center">
-          <h1 className="text-xl">{myPlant?.plantName}</h1>
+        <h1 className="text-2xl">{myPlant?.plantName}</h1>
+        <div className="flex-shrink-0" style={{ visibility: "hidden" }}>
+          {/* This is a placeholder to ensure the plant name is centered */}
+          <ChevronLeft />
         </div>
       </div>
+
+      <Card
+        sx={{
+          width: "100%",
+          maxWidth: "1400px",
+          maxHeight: "350px",
+          boxShadow: 0,
+          borderRadius: 0,
+          position: "fixed",
+          top: "100px",
+        }}
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={myPlant!.plantImage}
+            alt={myPlant!.plantName}
+            sx={{
+              height: "100%",
+              width: "100%",
+              maxHeight: "300px",
+              objectFit: "cover",
+            }}
+          />
+        </CardActionArea>
+      </Card>
+
+      <div>k</div>
+
       <BottomDetailSection />
     </div>
   );
