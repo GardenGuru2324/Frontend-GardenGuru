65e2ab9a145d96bca6e4316a655353e4b8c24b76
MilanMareels <milan.mareels@gmail.com>
2024-08-06 13:15:27 +0200

    PR feedback stzge two

--- src/components/myPlantDetails/overViewMyPlantDetails.tsx
+++ src/components/myPlantDetails/overViewMyPlantDetails.tsx
@@ -11,22 +11,30 @@ const OverviewMyPlantDetails = ({ myPlant }: OverviewMyPlantDetailsProps) => {
         Overview
       </div>
       <div className="m-4 flex flex-col gap-2">
-        <div className="flex justify-between w-full">
-          <p>Average height</p>
-          <p>{myPlant?.plantAvgHeight} cm</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p>Maximum height</p>
-          <p>{myPlant?.plantAvgHeight} cm</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p>Minimum temperature</p>
-          <p>{myPlant?.plantMinTemp} &#8451;</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p>Maximum temperature</p>
-          <p>{myPlant?.plantMaxTemp} &#8451;</p>
-        </div>
+        {myPlant.plantAvgHeight && (
+          <div className="flex justify-between w-full">
+            <p>Average height</p>
+            <p>{myPlant?.plantAvgHeight} cm</p>
+          </div>
+        )}
+        {myPlant.plantMaxHeight && (
+          <div className="flex justify-between w-full">
+            <p>Maximum height</p>
+            <p>{myPlant?.plantMaxHeight} cm</p>
+          </div>
+        )}
+        {myPlant.plantMinTemp && (
+          <div className="flex justify-between w-full">
+            <p>Minimum temperature</p>
+            <p>{myPlant?.plantMinTemp} &#8451;</p>
+          </div>
+        )}
+        {myPlant.plantMaxTemp && (
+          <div className="flex justify-between w-full">
+            <p>Maximum temperature</p>
+            <p>{myPlant?.plantMaxTemp} &#8451;</p>
+          </div>
+        )}
         {myPlant?.isVegetable ? (
           <div className="flex justify-between w-full">
             <p>Edible</p>
@@ -38,22 +46,35 @@ const OverviewMyPlantDetails = ({ myPlant }: OverviewMyPlantDetailsProps) => {
             <p>No</p>
           </div>
         )}
-        <div className="flex justify-between w-full">
-          <p>Growt habit</p>
-          <p>{myPlant?.plantGrowthHabit}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p>Growt rate</p>
-          <p>{myPlant?.plantGrowthRate}</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p>Days to harvest</p>
-          <p>{myPlant?.plantDaysToHarvest} days</p>
-        </div>
-        <div className="flex justify-between w-full">
-          <p>Plant row spacing</p>
-          <p>{myPlant?.plantRowSpacing} cm</p>
-        </div>
+        {myPlant.plantGrowthHabit && (
+          <div className="flex justify-between w-full">
+            <p>Growt habit</p>
+            <p>{myPlant?.plantGrowthHabit}</p>
+          </div>
+        )}
+        {myPlant.plantGrowthRate && (
+          <div className="flex justify-between w-full">
+            <p>Growt rate</p>
+            <p>{myPlant?.plantGrowthRate}</p>
+          </div>
+        )}
+        {myPlant.plantDaysToHarvest > 0 ? (
+          <div className="flex justify-between w-full">
+            <p>Days to harvest</p>
+            <p>{myPlant?.plantDaysToHarvest} days</p>
+          </div>
+        ) : (
+          <div className="flex justify-between w-full">
+            <p>Days to harvest</p>
+            <p>This plant cannot be harvested</p>
+          </div>
+        )}
+        {myPlant.plantRowSpacing && (
+          <div className="flex justify-between w-full">
+            <p>Plant row spacing</p>
+            <p>{myPlant?.plantRowSpacing} cm</p>
+          </div>
+        )}
       </div>
     </div>
   );
